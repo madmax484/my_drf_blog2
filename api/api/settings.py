5a8baa1143744f5cec8bc70106782c51a79fb710
madmax484 <madmax20@bk.ru>
2022-11-29 18:12:55 +0300

    v-0.3

--- api/api/settings.py
+++ api/api/settings.py
@@ -182,7 +182,12 @@ CKEDITOR_CONFIGS = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'levchenkomaksmsk@gmail.com'
+EMAIL_HOST_PASSWORD = 'tccagbpwejzgyqrc'
 
 
 
--- api/core/views.py
+++ api/core/views.py
@@ -1,7 +1,9 @@
+from django.core.mail import send_mail
 from rest_framework import viewsets, pagination, generics
+from rest_framework.views import APIView
 from taggit.models import Tag
 
-from .serializers import PostSerializer, TagSerializer
+from .serializers import PostSerializer, TagSerializer, ContactSerializer
 from .models import Post
 from rest_framework.response import Response
 from rest_framework import permissions
@@ -33,3 +35,23 @@ class TagView(generics.ListAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = [permissions.AllowAny]
+
+class AsideView(generics.ListAPIView):
+    queryset = Post.objects.all().order_by('-id')[:5]
+    serializer_class = PostSerializer
+    permission_classes = [permissions.AllowAny]
+
+class FeedBackView(APIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ContactSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer_class = ContactSerializer(data=request.data)
+        if serializer_class.is_valid():
+            data = serializer_class.validated_data
+            name = data.get('name')
+            from_email = data.get('email')
+            subject = data.get('subject')
+            message = data.get('message')
+            send_mail(f'От {name} | {subject}', message, from_email, ['levchenkomaksmsk@gmail.com'])
+            return Response({"success": "Sent"})
