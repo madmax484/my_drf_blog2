a1a9550236413e077a1b2c0f95de0bd5b97ef386
MaksLevchenko <111285984+MaksLevchenko@users.noreply.github.com>
2023-03-16 10:36:55 +0300

    Update permissions.py

--- api/core/permissions.py
+++ api/core/permissions.py
@@ -2,6 +2,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsAuthorOrStaffOrReadOnly(BasePermission):
+    """Разрешеия"""
     def has_object_permission(self, request, view, obj):
         return bool(
             request.method in SAFE_METHODS or
